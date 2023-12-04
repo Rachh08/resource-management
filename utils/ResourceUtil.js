cee0b7733d5e8e3dcc5e8b26dc1816e6d1162fad
Rachh08 <2200667J@student.tp.edu.sg>
2023-12-04 23:20:56 +0800

    updated codes

--- utils/ResourceUtil.js
+++ utils/ResourceUtil.js
@@ -5,6 +5,10 @@ async function viewResources(req, res) {
     try {
         const allResources = await readJSON('utils/resources.json');
         return res.status(201).json(allResources);
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+}
 
 async function addResource(req, res) {
     try {
